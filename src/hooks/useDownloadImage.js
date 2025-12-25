8b3eed22efab3472dd16a59db69a33d70d200d79
amirali72 <amirali531361@gmail.com>
2025-12-25 18:28:17 +0530

    new hook implemented to download chart

---
+++ src/hooks/useDownloadImage.js
@@ -0,0 +1,30 @@
+import html2canvas from 'html2canvas-pro';
+import React from 'react'
+
+export const useDownloadImage = async(printRef) => {
+    console.log("working");
+    
+    const element = printRef.current;
+    if (!element) return;
+
+    // Use html2canvas to render the element
+    const canvas = await html2canvas(element, {
+      allowTaint: true, // Option to handle cross-origin images
+    });
+
+    // Convert the canvas to a data URL (PNG format)
+    const dataUrl = canvas.toDataURL("image/png");
+
+    // Create a link element to trigger the download
+    const link = document.createElement("a");
+    link.href = dataUrl;
+    link.download = "component-screenshot.png"; // Specify the download file name
+
+    // Simulate a click to download the image
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  
+}
+
+
