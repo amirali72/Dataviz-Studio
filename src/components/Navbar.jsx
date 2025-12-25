6269f282d7c19fe6351fd65e57adfab04fbcd7f4
amirali72 <amirali531361@gmail.com>
2025-12-25 05:31:11 +0530

    Styled Navbar Component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,45 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
+import logo from "../assets/DataVizLogo.png";
+import { MdLightMode  } from "react-icons/md";
 
 const Navbar = () => {
   return (
     <div>
-      <nav className="flex">
-        <h1 className="text-3xl font-bold text-gray-800 mb-8">
-          DataViz Studio
-        </h1>
+      <nav className="flex justify-between  bg-white shadow-md mb-4">
+        <div className="left flex">
+          <img src={logo} alt="Logo" className="h-14" />
+          <h1 className="text-xl font-bold text-gray-800 self-center ">
+            DataViz Studio
+          </h1>
 
-        <ul className="nav-list">
-          <li className="nav-item">
-            <NavLink
-              to="/"
-              className={({ isActive }) =>
-                isActive ? "text-amber-500" : "text-black"
-              }
-            >
-              Builder
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink
-              to="/dashboard"
-              className={({ isActive }) =>
-                isActive ? "text-amber-500" : "text-black"
-              }
-            >
-              Dashboard
-            </NavLink>
-          </li>
-        </ul>
+          <ul className="flex self-center space-x-8 ml-10">
+            <li className="">
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? "text-amber-500 border-b-2 border-amber-500" : "text-black"
+                }
+              >
+                Builder
+              </NavLink>
+            </li>
+            <li className="">
+              <NavLink
+                to="/dashboard"
+                className={({ isActive }) =>
+                  isActive ? "text-amber-500 border-b-2 border-amber-500" : "text-black"
+                }
+              >
+                Dashboard
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+
+        <div className="right mr-4  self-center">
+          <MdLightMode size={30} className="text-amber-500" />
+        </div>
       </nav>
     </div>
   );
